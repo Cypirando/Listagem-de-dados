0aaa02752636ce7657fc85045efcc886a4c426af
cypirando <fabiocypriano@gmail.com>
2024-03-19 16:48:08 -0300

    feature: Adiciona banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,15 @@
-console.log('hello word ')
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get("/teste", () => {
+  return "Hello word";
+});
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("HTTP server running!");
+  });
