e9eeea1501d62e3cde19a86f448b855e2bde9ef9
cypirando <fabiocypriano@gmail.com>
2024-03-25 13:43:22 -0300

    feature: creating post

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import { sql } from './lib/postgres';
 
 const app = fastify();
 
-app.get('/links', async (request, reply) => {
+app.post('/links', async (request, reply) => {
   const { code, url } = z
     .object({
       code: z.string().min(3),
@@ -14,15 +14,15 @@ app.get('/links', async (request, reply) => {
 
   const result = await sql/*sql*/ `
       INSERT INTO short_links (code, original_url)
-      VALUES (${code} ${url})
+      VALUES (${code}, ${url})
       RETURNING id
     `;
 
   const link = result[0];
 
-  return reply.status(201);
+  return reply.status(201).send({ shortLinkId: link.id });
 });
-//1:20
+
 app
   .listen({
     port: 3333,
